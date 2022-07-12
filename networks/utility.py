71afb02e6a3828295b354de8428079323e6544d1
Victor S. Miller <algebravic@fb.com>
2022-07-12 16:40:55 -0700

    A function to convert a possibly non-standard sorting network
    into a standard one.

---
+++ networks/utility.py
@@ -0,0 +1,47 @@
+"""
+Utility functions for sorting networks
+"""
+
+from typing import List, Tuple
+from functools import partial
+
+def flip(fst: int, snd: int, trg: int) -> int:
+    """
+    Apply a transposition.
+    """
+    return fst if trg == snd else (snd if trg == fst else trg)
+
+def diff(tup1: Tuple[int], tup2: Tuple[int]) -> int:
+    """
+    Number of differences between corresponding elements
+    """
+    return sum((int(_[0] != _[1]) for _ in zip(tup1, tup2)))
+
+def standardize(net: List[Tuple[int, int]]) -> List[Tuple[int, int]]:
+    """
+    Knuth section 5.3.4 exercise 16, page 239
+    """
+    num = len(net)
+    changes = 0
+
+    while True:
+
+        # Find bad pair
+        place = None
+        for ind, (left, right) in net:
+            if left > right:
+                place = ind
+                lft, rgt = left, right
+                break
+
+        if place is None:
+            break
+        tfn = partial(flip, lft, rgt)
+
+        for jind in range(place, num):
+            val = tuple(map(tfn, net[jind]))
+            changes += diff(val, net[jind])
+            net[jind] = val
+
+    print(f"There were {changes} changes")
+        
